82d7a2618ba306867d1a2f309b64b852cb208e10
Dagal <dejarding@gmail.com>
2015-03-31 12:18:02 +0200

    First Commit

---
+++ src/c-dagal-tone.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * c-dagal-tone.h
+ * Copyright (C) 2015 Dagal <dejarding@gmail.com>
+ *
+ * audio is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * audio is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _C_DAGAL_TONE_H_
+#define _C_DAGAL_TONE_H_
+
+class CDagalTone: public Gtk::Window 
+{
+public:
+
+protected:
+
+private:
+
+};
+
+#endif // _C_DAGAL_TONE_H_
+
