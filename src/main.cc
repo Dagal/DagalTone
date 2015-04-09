a37e9c43481fc9802718023320b13dbf870f097d
Dagal <dejarding@gmail.com>
2015-04-09 06:07:55 +0200

    Correction de la création de l'instance de la classe principale.

--- src/main.cc
+++ src/main.cc
@@ -27,7 +27,7 @@ int main (int argc, char *argv[])
 {
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.dagal.dagaltone");
 
-	CDagalTone dagalTone;
+	CDagalTone dagalTone(app);
 
 	return app->run(dagalTone);
 }
