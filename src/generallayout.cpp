5c4a4d45e0faffb9af0a83aaa2f4c648121072d5
Dagal <dejarding@gmail.com>
2015-04-25 12:56:37 +0200

    Fichier .cc et .h renommé en .cpp et .hpp

---
+++ src/generallayout.cpp
@@ -0,0 +1,79 @@
+/* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 2; tab-width: 2 -*-  */
+/*
+ * generallayout.cc
+ * Copyright (C) 2015 Dejardin Gilbert <dejarding@gmail.com>
+ *
+ * audio is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * audio is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glibmm/main.h>
+#include "generallayout.h"
+
+GeneralLayout::GeneralLayout() :
+	Gtk::DrawingArea()
+{
+		mGM.moveTo(-0.5,-0.2);
+		
+		Glib::signal_timeout().connect(sigc::mem_fun(*this,&GeneralLayout::on_timeout),20);
+}
+
+GeneralLayout::~GeneralLayout()
+{
+}
+
+bool GeneralLayout::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+	// Récupération des informations de notre surface de travail
+	Gtk::Allocation allocation = get_allocation();
+	const int width = allocation.get_width();
+	const int height = allocation.get_height();
+
+	// Taille unitaire et centre au millieu de la surface
+	cr->scale(width,height);
+	cr->translate(0.5,0.5);
+
+	// Paramètres par défaut
+	cr->set_line_width(0.02);
+  cr->save();
+	cr->set_source_rgba(0.337,0.612,0.117,0.9);
+	cr->paint();
+	cr->restore();
+
+	// Dessin du fond d'écran
+	cr->arc(0, 0, 0.42, 0, 2*M_PI);
+	cr->save();
+	cr->set_source_rgba(1.0,1.0,1.0,0.8);
+	cr->fill_preserve();
+	cr->restore();
+	cr->stroke();
+
+	// Affichage des modules
+	mGM.update(20);
+	mGM.draw(cr);
+	
+	cr->clip();
+	
+	return true;
+}
+
+bool GeneralLayout::on_timeout()
+{
+	Glib::RefPtr<Gdk::Window> win = get_window();
+	if (win)
+	{
+		Gdk::Rectangle r(0,0, get_allocation().get_width(),get_allocation().get_height());
+		win->invalidate_rect(r, false);
+	}
+	return true;
+}
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 2; tab-width: 2 -*-  */
+/*
+ * main.cc
+ * Copyright (C) 2015 Dejardin Gilbert <dejarding@gmail.com>
+ * 
+ * dagaltone is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * dagaltone is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include <gtkmm/application.h>
+
+#include "c-dagal-tone.h"
+
+int main (int argc, char *argv[])
+{
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.dagal.dagaltone");
+
+	CDagalTone dagalTone(app);
+
+	return app->run(dagalTone);
+}
+
